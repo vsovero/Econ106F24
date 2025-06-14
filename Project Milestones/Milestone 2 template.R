e8e71fbf473d4046951c64a69d67b64ef6ba0c4c
vsovero <verosovero@gmail.com>
2025-06-13 17:32:00 -0700

    Add files via upload

---
+++ Project Milestones/Milestone 2 template.R
@@ -0,0 +1,56 @@
+##Template for Research Project Milestone 2
+
+
+#load necessary libraries
+library(tidyverse)
+
+##set your working directory to the location of your data file
+
+
+
+##read in your data file (easiest to load if it is saved as a csv file)
+
+
+
+#tidy your data as needed (join, reshape, etc.)
+
+
+
+##Categorical Data Cleaning####
+
+
+#categorical variable 1 (repeat for all 4 categorical variables)
+
+#convert to factor variable
+
+
+#report the current number of levels
+
+
+#list the current levels 
+
+
+#collapse the levels to no more than 5 
+
+
+#order the levels if the categorical data is ordinal
+
+
+#create a frequency table (dyplr)
+
+
+#create a bar plot (ordered by frequency if the categorical variable is not ordinal)
+
+
+##Quantitative Data Cleaning####
+
+
+#quantitative variable 1 (repeat for all 4 quantitative variables)
+
+#check how it's stored (character, numeric, etc.) and convert as needed
+
+
+#create a summary table with n, min, mean, median, and max (dplyr)
+
+
+#create a density plot or histogram
